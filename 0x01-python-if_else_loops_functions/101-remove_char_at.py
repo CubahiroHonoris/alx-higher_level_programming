ca3176a8af1a362c345f7a418ad610301eda7381
CubahiroHonoris <cubahiroalx@gmail.com>
2022-12-01 14:42:43 +0200

    deleting all

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    s = ""
-    i = 0
-    for c in str:
-        if i != n:
-            s += c
-        i += 1
-    return s
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    s = ""
-    for c in str:
-        if (97 <= ord(c) <= 122):
-            s += chr(ord(c) - 32)
-        else:
-            s += c
-    print("{:s}".format(s))
